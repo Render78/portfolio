1884e5f49135fde5dcc5e7cec0ab27de855796bb
Render78 <mariojuliantor@hotmail.com>
2024-05-07 11:34:07 -0300

    constants done

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `Esta es mi PC, donde siempre estoy trabajando y desarrollando proyectos utilizando muchas tecnologias. Como <b>frontend dev</b> utilizo HTML, CSS y JavaScript para construir interfaces atractivas, tambien React y Angular para desarrollar aplicaciones robustas.
+    En la parte de <b>backend</b>, utilizo C#, .NET CORE, NodeJS, MongoDB y gestores de base de datos SQL.
+    Mi computadora es la herramienta que utilizo todos los dias para seguir capacitandome y tambien para trabajar en mi mismo, tratando de subir un escalon nuevo cada dia.
+    Aqui puedes ver mi <a href="https://github.com/Render78" target="_blank">Github</a> y mi <a href="https://www.linkedin.com/in/mario-juli%C3%A1n-molina-torres-2a8bb7247/" target="_blank">LinkedIn</a>!`,
+    "cs-degree": `Aqui tengo mis certificados y proximamente estara mi futuro titulo de <b>Tecnico Universitario en Programacion</b>. Me gusta tenerlos en la pared porque no fueron logros faciles y cada dia me motivan a seguir adelante en este maravilloso camino.`,
+    "sofa-table": `Este es mi sofa donde me gusta relajarme todos los dias luego de largas sesiones de trabajo, estudio y/o codeo.`,
+    tv: `Esta es mi TV donde suelo ver peliculas, series o juego videojuegos. Disfrutar de contenido multimedia es uno de mis pasatiempos favoritos que disfruto todos los dias.`,
+    bed: `Este es mi lugar de descanso donde siempre tengo una libreta para escribir las nuevas ideas que se me vienen a la cabeza a la hora de acostarme y relajarme. Todas las noches pienso en que nuevos proyectos puedo realizar para seguir fortaleciendo mi logica de desarrollo.`,
+    resume: `Aqui esta mi Curriculum Vitae (CV). Puedes verlo y analizarlo haciendo <a href="https://flowcv.com/resume/lomsbfkiis" target="_blank">click aqui</a>! <br> <br> El documento de la derecha es informacion sobre mis proyectos!`,
+    projects: `Este es mi portfolio desarrollado con HTML, CSS y JavaScript, usando las librerias <b>KaboomJS</b> y <b>Vite</b>. Pense en hacer un "videojuego interactivo" ya que es lo que mas me gusta disfrutar en el dia a dia. Para ver todos mis proyectos realizados hasta la fecha, puedes ir a mis <a href="https://github.com/Render78?tab=repositories" target="_blank">repositorios de GitHub!</a> <br> <br> El documento de la izquierda es informacion sobre mi Curriculum Vitae (CV)!`,
+    library: `Esta es mi coleccion de libros. En ella hay variedad de temas como tecnologia, software dev, finanzas, lenguas, etc. Gracias a ellos aprendi muchas herramientas y habilidades, como por ejemplo, el poder hablar ingles y llevarlo hacia un nivel B1.<br> <br>Leer libros es una de mis actividades favoritas!`,
+    exit: `Cierra la ventana del navegador para salir.`,
+};
